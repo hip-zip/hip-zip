86debbe41294d76cb1e9c20da9fd27d3bc8d9d48
thdwoqor <celebarte01@gmail.com>
2023-12-14 20:49:34 +0900

    init commit

---
+++ src/main/java/com/example/hipzip/HipZipApplication.java
@@ -0,0 +1,13 @@
+package com.example.hipzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HipZipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HipZipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hipzip/HipZipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hipzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HipZipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
