32fac07bd77cdb86b9aa8a8b2432fd09175521cc
thdwoqor <celebarte01@gmail.com>
2023-12-27 01:41:11 +0900

    feat: ArtistListResponse id 추가

--- src/main/java/com/example/hipzip/application/dto/ArtistListResponse.java
+++ src/main/java/com/example/hipzip/application/dto/ArtistListResponse.java
@@ -3,11 +3,12 @@ package com.example.hipzip.application.dto;
 import com.example.hipzip.domain.artist.Artist;
 
 public record ArtistListResponse(
+        Long id,
         String name,
         String image
 ) {
 
     public static ArtistListResponse from(Artist artist) {
-        return new ArtistListResponse(artist.getName(), artist.getImage());
+        return new ArtistListResponse(artist.getId(), artist.getName(), artist.getImage());
     }
 }
