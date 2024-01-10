ff3f2409d42bc11f5328dc946b4d4ae2404ec86a
thdwoqor <celebarte01@gmail.com>
2024-01-10 15:04:33 +0900

    feat: 테스트 코드 수정

--- src/test/java/com/example/hipzip/ui/ArtistControllerTest.java
+++ src/test/java/com/example/hipzip/ui/ArtistControllerTest.java
@@ -4,12 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.example.hipzip.application.ArtistService;
 import com.example.hipzip.application.dto.ArtistListResponse;
+import com.example.hipzip.application.dto.ArtistModifyRequest;
 import com.example.hipzip.domain.artist.Artist;
 import com.example.hipzip.domain.artist.ArtistFixture;
 import com.example.hipzip.domain.artist.ArtistRepository;
 import com.example.hipzip.domain.artist.HashTagRepository;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -73,9 +75,17 @@ class ArtistControllerTest {
         Long 이서_ID = artistService.saveArtist(ArtistFixture.이서_저장_요청());
         Long 아이브_ID = artistService.saveArtist(ArtistFixture.아이브_저장_요청());
 
+        ArtistModifyRequest artistModifyRequest = new ArtistModifyRequest(
+                아이브_ID,
+                "IVE",
+                "https://image.blip.kr/v1/file/f17114ade66730456e304bef23258bb6",
+                List.of("아이브", "ive", "IVE"),
+                List.of(이서_ID)
+        );
+
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
-                .body(ArtistFixture.아이브_수정_요청())
+                .body(artistModifyRequest)
                 .when().put("/artists")
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value());
@@ -84,9 +94,9 @@ class ArtistControllerTest {
         Artist 아이브 = artistRepository.findById(아이브_ID).orElseThrow();
 
         assertAll(
-                () -> Assertions.assertThat(이서.getGroup().getId()).isEqualTo(ArtistFixture.아이브_수정_요청().id()),
-                () -> Assertions.assertThat(아이브.getName()).isEqualTo(ArtistFixture.아이브_수정_요청().name()),
-                () -> Assertions.assertThat(아이브.getImage()).isEqualTo(ArtistFixture.아이브_수정_요청().image())
+                () -> Assertions.assertThat(이서.getGroup().getId()).isEqualTo(artistModifyRequest.id()),
+                () -> Assertions.assertThat(아이브.getName()).isEqualTo(artistModifyRequest.name()),
+                () -> Assertions.assertThat(아이브.getImage()).isEqualTo(artistModifyRequest.image())
         );
     }
 }
