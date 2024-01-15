bbdf0f58d1744ff01d7edcd42a381b64ee42b953
thdwoqor <celebarte01@gmail.com>
2024-01-15 17:32:03 +0900

    chore: 테이블명 변경

--- src/main/resources/artist.sql
+++ src/main/resources/artist.sql
@@ -9,24 +9,23 @@ create table artist
     updated_at  timestamp(6)
 );
 
-create table artist_hash_tag
-(
-    id          bigint auto_increment primary key,
-    artist_id   bigint,
-    hash_tag_id bigint,
-    name        varchar(255),
-    created_at  timestamp(6),
-    updated_at  timestamp(6),
-    constraint fk_artist_hash_tag_artist_id foreign key (artist_id) references artist (id),
-    constraint fk_artist_hash_tag_hash_tag_id foreign key (hash_tag_id) references hash_tag (id)
-
-);
-
-create table hash_tag
+create table hashtag
 (
     id         bigint auto_increment primary key,
     name       varchar(255),
     created_at timestamp(6),
     updated_at timestamp(6)
+);
+
+create table artist_hashtag
+(
+    id          bigint auto_increment primary key,
+    artist_id   bigint,
+    hashtag_id bigint,
+    name        varchar(255),
+    created_at  timestamp(6),
+    updated_at  timestamp(6),
+    constraint fk_artist_hashtag_artist_id foreign key (artist_id) references artist (id),
+    constraint fk_artist_hashtag_hashtag_id foreign key (hashtag_id) references hashtag (id)
 );
 
