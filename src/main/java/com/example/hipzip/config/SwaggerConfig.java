6eec3565d6b71afd768c6490b5ffdf2402a5476a
thdwoqor <celebarte01@gmail.com>
2024-01-22 13:47:33 +0900

    feat: swagger에서 https를 지원하도록 수정

--- src/main/java/com/example/hipzip/config/SwaggerConfig.java
+++ src/main/java/com/example/hipzip/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.example.hipzip.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,14 +11,14 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
-    }
 
-    private Info apiInfo() {
-        return new Info()
+        Info info = new Info()
                 .title("Api Documentation")
                 .description("Api Documentation");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info)
+                .addServersItem(new Server().url("/"));
     }
 }
