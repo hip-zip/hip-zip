4e8533ff99a26afed88eae6e71c582ab51b69650
thdwoqor <celebarte01@gmail.com>
2024-02-07 14:54:07 +0900

    feat: LocalDateTime -> LocalDate 변경

--- src/test/java/com/example/hipzip/domain/AlbumFixture.java
+++ src/test/java/com/example/hipzip/domain/AlbumFixture.java
@@ -12,7 +12,7 @@ public class AlbumFixture {
         return Album.builder()
                 .name("WAVE")
                 .image("https://cdnimg.melon.co.kr/cm2/album/images/112/53/005/11253005_20230530104634_500.jpg?e7c6924554495111b3c75ef13b53a251/melon/resize/282/quality/80/optimize")
-                .releaseDate(LocalDateTime.of(2023, 5, 31, 0, 0))
+                .releaseDate(LocalDate.of(2023, 5, 31))
                 .musicVideo("https://youtu.be/qD1kP_nJU3o?si=2aXe5_eU2goMOTOG")
                 .artist(artist)
                 .build();
