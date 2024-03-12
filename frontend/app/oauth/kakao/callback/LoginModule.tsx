328fd2ada0f8e940dfcb8ee423c5f148cb2f9704
디디 <126233367+devcdd@users.noreply.github.com>
2024-03-12 18:37:30 +0900

    상용 대비 로그인 로직 수정 (#4)
    * Fix: debounce 로직 구현 중
    
    * Feat: Debounce 로직 구현 완료
    
    * Fix: 회사 컴퓨터 푸쉬 테스트
    
    * Fix: 회사 컴퓨터 푸쉬 테스트
    
    * Feat: 좋아요 이펙트 추가
    
    * Feat: 좋아요 시 로그인 확인 로직
    
    * Fix: Type Naming Convention Fix
    
    * Fix: 메인 페이지 UI 변경
    
    * Fix: Fix build Errors
    
    * Fix: Fix build Errors Complete
    
    * Fix: Login Token 관리 로직 변경
    
    * Feat: 로그인 유저 정보 표시기능 추가
    
    * Fix: 빌드 타입 에러 수정
    
    * Fix: 상용 에러 수정
    
    * Fix: 로그인 로직 수정

--- frontend/app/oauth/kakao/callback/LoginModule.tsx
+++ frontend/app/oauth/kakao/callback/LoginModule.tsx
@@ -4,6 +4,8 @@ import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { setToken, useTokenStore } from "@/app/store/useTokenStore";
 import { getUserInfo } from "@/app/api/Server/requests";
+import { setUserInfo } from "@/app/store/useUserInfoStore";
+import { useCookies } from "react-cookie";
 
 interface LoginModuleProps {
   token: string;
@@ -11,10 +13,23 @@ interface LoginModuleProps {
 
 const LoginModule = (props: LoginModuleProps) => {
   const router = useRouter();
+  const [cookies, setCookie, removeCookie] = useCookies(["token"]);
+
+  const tokenValidation = async () => {
+    try {
+      const response = await getUserInfo();
+      setCookie("token", props.token, { path: "/" });
+      setToken(props.token);
+      setUserInfo(response);
+      router.push("/main");
+    } catch (e) {
+      router.push("/");
+      console.log("LoginModule.tsx:21 -  = BackEnd Response Error");
+    }
+  };
 
   useEffect(() => {
-    setToken(props.token);
-    router.push("/main");
+    tokenValidation();
   }, []);
 
   return <div className={"h-screen"}></div>;
