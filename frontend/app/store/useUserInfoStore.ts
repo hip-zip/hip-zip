fa3093a9eef3834798c348bb21f9d6bbcf3681b3
devcdd <developer.cdd@gmail.com>
2024-03-12 17:59:50 +0900

    Fix: 빌드 타입 에러 수정

--- frontend/app/store/useUserInfoStore.ts
+++ frontend/app/store/useUserInfoStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 
-interface UserInfo {
+export interface UserInfo {
   email: string;
   image: string;
   nickName: string;
